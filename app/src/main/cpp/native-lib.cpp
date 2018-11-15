8559a9a8530711a47a1aa63cdef785e5e82e602f
Patrick Mutwiri <patwiri@gmail.com>
2018-11-15 16:15:12 +0300

    calc, simple

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_pmutwiri_myfirstapp_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello World from C++";
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/example/pmutwiri/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/pmutwiri/myfirstapp/MainActivity.kt
@@ -2,16 +2,18 @@ package com.example.pmutwiri.myfirstapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log;
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    val MY_TAG : String = "my_custom tag"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Log.i(MY_TAG,"onCreate")
         // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+      //  sample_text.text = stringFromJNI()
     }
 
     /**
